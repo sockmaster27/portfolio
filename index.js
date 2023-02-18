0aa83c319253295a0ce79ed740b4c5800bb45436
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2023-02-19 00:30:30 +0100

    Compress video

--- index.js
+++
@@ -1,7 +0,0 @@
-// Load appropriate video resolution
-const vertical_thresh = 1400;
-const video = document.getElementById("waves");
-const source = document.createElement("source");
-source.src = video.clientHeight < vertical_thresh ? "resources/Waves-1080p.mp4" : "resources/Waves-4K.mp4";
-source.type = 'video/mp4';
-video.appendChild(source);
